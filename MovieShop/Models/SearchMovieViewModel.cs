4d30f2c032f00ed70082b7d3a0e60b26b5da2641
Manh <manh.nguyenba.vn@gmail.com>
2021-09-13 08:45:05 +0700

    Search a Product

---
+++ MovieShop/Models/SearchMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.Models
+{
+    public class SearchMovieViewModel
+    {
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
---
+++ MovieShop/ViewComponents/SearchMovie.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieShop.Data;
+using MovieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.ViewComponents
+{
+    public class SearchMovie : ViewComponent
+    {
+        private readonly MVCMovieContext _context;
+
+        public SearchMovie(MVCMovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        )
+        {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            //-------------------------------------------------------------------------------------
+            var movies = from m in _context.Movie
+                         select m;
+            
+            var movieGenreVM = new SearchMovieViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                SearchString = "",
+                MovieGenre = ""
+            };
+            return View(movieGenreVM);
+        }       
+
+    }
+}
